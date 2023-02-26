094a4d044e24da2474cd5610d309ee95a6441e64
Syed Hashir Ali <113060592+Syed-hashir@users.noreply.github.com>
2023-02-26 12:41:00 +0500

    Add files via upload
    Taking Input and Output

---
+++ input_output.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int num1,num2,num3,sum=0;
+    cout<<"Enter a Number"<<endl;
+    cin>>num1>>num2>>num3>>sum;
+    sum=num1+num2+num3;
+    cout<<num1<<num2<<num3<<" "<<sum<<endl;
+    return 0;
+}
